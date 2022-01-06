1ac6fe765385dca800835c3969c1c63634b2c41d
sumiya12 <sumiya.udval@gmail.com>
2022-01-06 23:36:01 +0800

    lesson 84

---
+++ app.js
@@ -0,0 +1,12 @@
+var name = "Болдоо";
+var age = 33;
+console.log(name + " нэртэй хүн " + age + " настай.");
+
+{
+  let name = "Цэцгээ";
+  let age = 21;
+  const address = "ub";
+  console.log(name + " нэртэй хүн " + age + " настай.");
+}
+
+console.log(name + " нэртэй хүн " + age + " настай.");
